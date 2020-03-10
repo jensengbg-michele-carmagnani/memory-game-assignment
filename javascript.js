1d48fc2d65857dff74dc59772aef001c26adf135
Michele <michele.carmagnani@gmail.com>
2020-03-10 13:13:14 +0100

    game almost complete

--- javascript.js
+++ javascript.js
@@ -2,34 +2,37 @@
 
 
 // calling all the elements Card
-let card = document.querySelectorAll('.card');
-let card = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
-console.log('card before shuffle: ', card);
+let cards = document.querySelectorAll('.card');
+let cardNumber = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+//  console.log('card before shuffle: ', cardNumber[3]);
  shuffel();
- console.log('card after shuffle: ', card);
+ //console.log('card after shuffle: ', cardNumber);
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBord = false;
 
 //shuffel  the array 
 function shuffel() {
 	//console.log('card before shuffle: ', card);
-	for (let i = card.length-1; i >= 0; i--) {
+	for (let i = cardNumber.length-1; i >= 0; i--) {
        // console.log('-----------iteration ' + i + '---------');
         let randomIndex = Math.floor(Math.random() * (i + 1));
         //console.log('RandomIndex: ', randomIndex);
-        let itemAtIndex = card[randomIndex];
+        let itemAtIndex = cardNumber[randomIndex];
         //console.log('itemAtIndex: ', itemAtIndex);
-        card[randomIndex] = card[i];
+        cardNumber[randomIndex] = cardNumber[i];
        // console.log('Card[0]: ', card[0]);
         // console.log('Still RandomIndex: ', randomIndex);
         // console.log('Card[i] before shuffle: ', card[i]);
         // console.log('card[randomIndex]', card[randomIndex]);
-        card[i] = itemAtIndex;
+        cardNumber[i] = itemAtIndex;
         // console.log('card[i]', card[i]);
         // console.log('Card after shuffle: ', card);
         // console.log('-----------iteration---------');
     }
     
-    return card;
+    return cardNumber;
   
 }	
 
@@ -39,39 +42,78 @@ function shuffel() {
 // Assign number(create a h3 to append to the card)
 assignNuber();
 function assignNuber(){
-for (let i= 0; i < cardNumb.length; i++){
-    let cardList = document.createElement('h3');
-    cardList.innerHTML = cardNumb[i];
-    cardElemnt[i].appendChild(cardList);
-    console.log('cards number', cardList)
+for (let i= 0; i < cardNumber.length; i++){
+    let number = document.createElement('h3');
+    number.innerHTML = cardNumber[i];
+    //console.log('cardList: ', number);
+   // console.log('cards[i]: ', cards[i]);
+    cards[i].appendChild(number);
+    cards[i].setAttribute('data-framework', cardNumber[i]);
+   // console.log('cards number', number)
     }
-return cardNumb
+return cardNumber   
 }
 
 
 
-//console.log('cards array', deckElemnt );
-flipCard();
-console.log(card)
-function flipCard(){
-for (i = 0; i < cards.length; i++){
-    cards[i].addEventListener('click', function(event) {
-        event.target.classList.toggle('fornt-card');
-        console.log('cards array', cards );
-        if (!hasFlippedCard) {
-            if (lockBoard) return;
-            hasFlippedCard = true;
-            firstCard = this;
-        } else {
-            hasFlippedCard = false;
-            secondCard = this;
-            checkForMatch()
-        }
-    });
+
+// flip the card function 
+
+function flipCard(event) {
+  console.log('firstCard: ', firstCard);
+  console.log('secondCard: ', secondCard);
+  if (!firstCard || !secondCard) {
+    event.target.classList.toggle('front');
+    //lock the bord 
+    if (lockBord) return; 
+    if (this === firstCard) return;
+
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = this;  
+      return;    
+    }
+    secondCard = this;
     
-    }
-    return flipCard;
-}
+
+    checkForMatch();
+  }
+ }
+
+ function checkForMatch() {
+   if (firstCard.dataset.framework === secondCard.dataset.framework) {
+     disableCards();
+     return;
+   }
+
+unflipCards();
+ }
+
+ function disableCards() {
+   firstCard.removeEventListener('click', flipCard);
+   secondCard.removeEventListener('click', flipCard);
+   resetBoard();
+ }
+
+ function unflipCards() {
+     lockBord = true;
+   setTimeout(() => {
+     firstCard.classList.remove('front');
+     secondCard.classList.remove('front');
+     resetBoard();
+   }, 1500);
+   
+ }
+
+ function resetBoard(){
+     [hasFlippedCard, lockBord] = [false,false];
+     [firstCard, secondCard] = [null, null];
+ }
+
+  cards.forEach(card => card.addEventListener('click', flipCard));
+  
+
+
 
 
 
